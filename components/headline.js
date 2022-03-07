1a5349ac67ec5a988422d2a7540a105f952fbe11
Isaac Murtagh <isaac.murtagh@centrapay.com>
2022-03-07 17:52:45 +1300

    remove exmaples for paid work

--- components/headline.js
+++ components/headline.js
@@ -3,7 +3,7 @@ export default function ({ title, subtitle, caption }) {
     <div className="flex flex-col text-center max-w-4xl mx-auto mb-8 md:mb-16">
     { caption && <span className="uppercase text-rose-500 mb-1">{ caption }</span> }
     { title && <h1 className="text-4xl font-bold mb-4">{ title }</h1> }
-    { subtitle && <h2 className="text-gray-500 text-xl font-extralight max-w-2xl mx-auto">{ subtitle }</h2> }
+    { subtitle && <h2 className="text-gray-500 text-xl font-extralight max-w-2xl mx-auto" dangerouslySetInnerHTML={{ __html: subtitle }}/> }
   </div>
   )
 }
